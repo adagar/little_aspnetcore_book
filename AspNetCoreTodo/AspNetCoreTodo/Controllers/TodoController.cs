3790e76482675f7a9c09dad833a2f7d554f7b7af
Adalberto N Garza <adagar@users.noreply.github.com>
2018-06-29 10:13:16 -0400

    Working on MVC

---
+++ AspNetCoreTodo/AspNetCoreTodo/Controllers/TodoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreTodo.Controllers
+{
+    public class TodoController : Controller
+    {
+        public IActionResult Index()
+        {
+            //get to-do tiems from database
+
+            //put items into a model
+
+            //render view using the model
+        }
+    }
+}
---
+++ AspNetCoreTodo/AspNetCoreTodo/Models/TodoItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreTodo.Models
+{
+    public class TodoItem
+    {
+        public Guid Id { get; set; }
+
+        public bool IsDone { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public DateTimeOffset? DueAt { get; set; } //? means that this propery is nullable/optional
+    }
+}
