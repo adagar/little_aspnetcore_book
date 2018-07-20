b4abf8acb04cfcc68f302b3d579c42621576cc81
Adalberto 'Nick' Garza <adagar121987@gmail.com>
2018-07-19 20:33:10 -0400

    Bookmark - Page 102
    Having error with new testing project, may clear and restart

---
+++ AspNetCoreTodo/AspNetCoreTodo.UnitTests/TodoItemServiceShould.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Data;
+using AspNetCoreTodo.Models;
+using AspNetCoreTodo.Services;
+//using Microsoft.EntityFrameworkCore;;
+using Xunit;
+
+namespace AspNetCoreTodo.UnitTests
+{
+    public class TodoItemServiceShould
+    {
+    }
+}
