cbbbff8b7067bd31d37305e2b3843b0a35689f37
Adalberto 'Nick' Garza <adagar121987@gmail.com>
2018-07-21 13:16:56 -0400

    Bookmark - Page 105

--- AspNetCoreTodo/AspNetCoreTodo.UnitTests/TodoItemServiceShould.cs
+++ AspNetCoreTodo/AspNetCoreTodo.UnitTests/TodoItemServiceShould.cs
@@ -1,16 +1,55 @@
 ﻿using System;
+using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Text;
-using System.Threading.Tasks;
 using AspNetCoreTodo.Data;
 using AspNetCoreTodo.Models;
 using AspNetCoreTodo.Services;
-//using Microsoft.EntityFrameworkCore;;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace AspNetCoreTodo.UnitTests
 {
     public class TodoItemServiceShould
     {
+        [Fact]
+        public async Task AddNewItemAsIncompleteWithDueDate()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "Test_AddNewItem").Options;
+
+            //setup a context for writing
+            using (var context = new ApplicationDbContext(options))
+            {
+                var service = new TodoItemService(context);
+
+                var fakeUser = new ApplicationUser
+                {
+                    Id = "fake-000",
+                    UserName = "fake@example.com"
+                };
+
+                await service.AddItemAsync(new TodoItem
+                {
+                    Title = "Testing?"
+                }, fakeUser);
+            }
+
+            //using a separate context to read data back from the fake DB
+            using (var context = new ApplicationDbContext(options))
+            {
+                var itemsInDatabase = await context
+                    .Items.CountAsync();
+                Assert.Equal(1, itemsInDatabase);
+
+                var item = await context.Items.FirstAsync();
+                Assert.Equal("Testing?", item.Title);
+                Assert.Equal(false, item.IsDone);
+
+                //item should be due 3 days from now (approx)
+                var difference = DateTimeOffset.Now.AddDays(3) - item.DueAt;
+                Assert.True(difference < TimeSpan.FromSeconds(1));
+            }
+        }
     }
 }
